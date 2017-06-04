148c3fbb74405ef4beb5d13e02e83c17bf9f2781
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-06-04 22:40:42 +0100

    Fix order of params

--- lib/object_comparator/minitest.rb
+++ lib/object_comparator/minitest.rb
@@ -6,14 +6,14 @@ class ObjectComparator
       obj1 = InspectionString.new(obj1.inspect)
       obj2 = InspectionString.new(obj2.inspect)
 
-      assert_equal(obj2, obj1, message)
+      assert_equal(obj1, obj2, message)
     end
 
     def refute_equal_objects(obj1, obj2, message = nil)
       obj1 = InspectionString.new(obj1.inspect)
       obj2 = InspectionString.new(obj2.inspect)
 
-      refute_equal(obj2, obj1, message)
+      refute_equal(obj1, obj2, message)
     end
   end
 end
